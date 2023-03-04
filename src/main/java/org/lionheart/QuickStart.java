9d4bad2111510ef656851108c87165f20be6ede6
Gianni Fontanot <giannifontanot@gmail.com>
2023-03-03 21:29:41 -0600

    Quickstart class is now retrieving data from a Mongo database located in Altas.

--- src/main/java/org/lionheart/QuickStart.java
+++ src/main/java/org/lionheart/QuickStart.java
@@ -12,13 +12,13 @@ public class QuickStart {
     public static void main( String[] args ) {
 
         // Replace the placeholder with your MongoDB deployment's connection string
-        String uri = "<connection string uri>";
+        String uri = "mongodb+srv://root:root1234@cluster0.antyn.mongodb.net/?retryWrites=true&w=majority";
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
+            MongoDatabase database = mongoClient.getDatabase("travel-blog-db");
+            MongoCollection<Document> collection = database.getCollection("trips");
 
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            Document doc = collection.find(eq("tripTitle", "Paris")).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
             } else {
