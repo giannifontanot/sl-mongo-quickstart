d974a143530f0ea4627f4b475f831207d1f57cfb
Gianni Fontanot <giannifontanot@gmail.com>
2023-03-04 17:24:50 -0600

    Display the Planets collection from Atlas by using a FindIterable object.

---
+++ src/main/java/org/lionheart/ReadPlanetData.java
@@ -0,0 +1,36 @@
+package org.lionheart;
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import org.bson.conversions.Bson;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+/*
+* What I learned?
+* I retrieved the entire collection of planets from Atlas,
+* Store the search in a FindIterable
+* and display the contents in console
+* */
+
+public class ReadPlanetData {
+    public static void main( String[] args ) {
+
+        String uri = "mongodb+srv://root:root1234@cluster0.antyn.mongodb.net/?retryWrites=true&w=majority";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_guides");
+            MongoCollection<Document> collection = database.getCollection("planets");
+
+            List<Document> planets =  new ArrayList<>();
+
+            FindIterable <Document> iterable = collection.find();
+            iterable.into(planets);
+            System.out.println(planets);
+        }
+    }
+}
