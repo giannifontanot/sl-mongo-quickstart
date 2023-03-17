30186df424146a623956c7b0572bb8743749fdbd
giannifontanotAA <93943543+giannifontanotAA@users.noreply.github.com>
2023-03-16 22:51:58 -0500

    Read one record from DB.

--- src/main/java/org/lionheart/FindOneShipwreck.java
+++ src/main/java/org/lionheart/FindOneShipwreck.java
@@ -4,21 +4,32 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import static com.mongodb.client.model.Filters.eq;
 
 
 public class FindOneShipwreck {
 
     public static void main(String[] args){
 
-        String mongoUri = "mongodb+srv://root:<password>@cluster0.antyn.mongodb.net/?retryWrites=true&w=majority";
+        String mongoUri = "mongodb+srv://root:root1234@cluster0.antyn.mongodb.net/?retryWrites=true&w=majority";
 
         try(MongoClient mongoClient = MongoClients.create(mongoUri)){
-            MongoDatabase database = mongoClient.getDatabase("sample_geospatial");
-            MongoCollection<Document> collection =  database.getCollection("shipwrecks");
+            MongoDatabase database = mongoClient.getDatabase("sample_guides");
+            MongoCollection<Document> collection =  database.getCollection("planets");
 
 
-            Document doc = collection.find();
-        }
-    }
-}
+            Bson projectionFields = Projections.fields(
+                    Projections.include("name", "orderFromSun", "hasRings"),
+                    Projections.excludeId());
+
+            Document doc = collection.find(eq("name", "Mars"))
+                    .projection(projectionFields)
+                    .sort(Sorts.ascending("orderFromSun"))
+                    .first();
+            System.out.println(doc.toJson());
+}}}
